57226bc40d79811dc34d13b9cf03bb782d58c267
ianwhale <whalenia@msu.edu>
2017-11-13 04:10:41 +0000

    Updated a few things...

--- classify.py
+++ classify.py
@@ -21,9 +21,10 @@ def get_split_indices(n, split):
 
 
 def classify():
-    batch_size = 1
-    cuda = False
+    batch_size = 256
+    cuda = True
     validation_split = 0.25
+    fine_tune = False
 
     print("Constructing train_loader...")
 
@@ -55,15 +56,16 @@ def classify():
 
     num_classes = train_loader.dataset.num_categories
 
-    model = torchvision.models.resnet18(pretrained=True)
+    model = torchvision.models.resnet152(pretrained=True)
 
-    for param in model.parameters():
-        param.requires_grad = False
+    if not fine_tune:
+        for param in model.parameters():
+            param.requires_grad = False
 
     num_filters = model.fc.in_features
     model.fc = nn.Linear(num_filters, num_classes)  # Resize output layer.
 
-    learning_rate = 1e-3
+    learning_rate = 5e-2
     momentum = 0.9
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.fc.parameters(), lr=learning_rate, momentum=momentum)
@@ -80,11 +82,15 @@ def classify():
 
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
+    print("Output classes: {}".format(train_loader.dataset.num_categories))
+
     # Code adapated from: http://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
     since = time.time()
 
     best_model_wts = model.state_dict()
     best_acc = 0
+
+    log_interval = 100 # Report stats every 100 minibatches.
 
     for epoch in range(epochs):
         print("Epoch {} / {}".format(epoch, epochs - 1))
@@ -100,6 +106,8 @@ def classify():
 
             running_loss = 0.0
             running_corrects = 0
+
+            counter = 0
 
             for data in dataloaders[phase]:
                 inputs, labels = data
@@ -124,6 +132,15 @@ def classify():
 
                 running_loss += loss.data[0]
                 running_corrects += torch.sum(preds == labels.data)
+
+                if phase == "train" and counter != 0 and counter % log_interval == 0:
+                    print("Training loss at iteration {}: {:.4f}".format(counter,
+                        running_loss / (counter * batch_size)))
+                    print("Training accuracy at iteration {}: {:.4f}".format(counter,
+                        running_corrects / (counter * batch_size)))
+
+
+                counter += 1
 
             epoch_loss = running_loss / len(data_loaders[phase].dataset)
             epoch_acc = running_corrects / len(data_loaders[phase].dataset)
