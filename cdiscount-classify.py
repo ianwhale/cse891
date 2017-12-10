b3bc373d145ccfbd0891cb8a51cc2d89fbe70bd3
Matthew Andres Moreno <matthew.andres.moreno@gmail.com>
2017-12-10 13:17:26 -0500

    Implement framework for training all four models
    We had to change the doubledataset loader to only load certain data
    based on whether it was to be used for the binary, flat, meuble, or
    electronique datasets.

---
+++ cdiscount-classify.py
@@ -0,0 +1,168 @@
+import time
+import argparse
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from random import shuffle
+from torch.autograd import Variable
+from cdiscountdataset import CDiscountDataSet
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+from collections import defaultdict
+
+from doubledataset import DoubleDataSet as DDS
+
+def get_split_indices(n, split):
+    idx = [i for i in range(n)]
+    split = int(n * split)
+
+    shuffle(idx)
+
+    return idx[0: n - split], idx[n - split:]
+
+def load_data(which):
+    batch_size = 256
+    validation_split = 0.25
+
+    furniture = "./data/meuble_data.bson"
+    electronics = "./data/electronique_data.bson"
+
+    tset = DDS(furniture, electronics, which)
+
+    num_classes = tset.num_classes
+
+    train_idx, valid_idx = get_split_indices(len(tset), validation_split)
+
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    train_loader = DataLoader(tset, sampler=train_sampler, batch_size=batch_size)
+    valid_loader = DataLoader(tset, sampler=valid_sampler, batch_size=batch_size)
+
+    dataloaders = {
+            "train": train_loader,
+            "val": valid_loader
+    }
+
+    print("Done.")
+
+    return dataloaders, num_classes, batch_size
+
+def classify(which, pretrain):
+
+    cuda = False
+    fine_tune = False
+
+    dataloaders, num_classes, batch_size = load_data(which)
+
+    model = torchvision.models.resnet18(pretrained=pretrain)
+
+    if not fine_tune:
+        for param in model.parameters():
+            param.requires_grad = False
+
+    num_filters = model.fc.in_features
+    model.fc = nn.Linear(num_filters, num_classes)  # Resize output layer.
+
+    learning_rate = 5e-2
+    momentum = 0.9
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.fc.parameters(), lr=learning_rate, momentum=momentum)
+
+    epochs = 10
+    log_interval = 1
+
+    print("Loading model to Cuda...")
+
+    if cuda:
+        criterion, model = criterion.cuda(), model.cuda()
+
+    print("Done.")
+
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+
+    # Code adapated from: http://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
+    since = time.time()
+
+    best_model_wts = model.state_dict()
+    best_acc = 0
+
+    for epoch in range(epochs):
+        print("Epoch {} / {}".format(epoch, epochs - 1))
+        print("-" * 10)
+
+        for phase in ["train", "val"]:
+            if phase == "train":
+                exp_lr_scheduler.step()
+                model.train(True)
+
+            else:
+                model.train(False)
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            counter = 0
+
+            for data in dataloaders[phase]:
+                inputs, labels = data
+
+                if cuda:
+                    inputs = Variable(inputs.cuda())
+                    labels = Variable(labels.cuda())
+
+                else:
+                    inputs = Variable(inputs)
+                    labels = Variable(labels)
+
+                optimizer.zero_grad()
+
+                outputs = model(inputs)
+                _, preds = torch.max(outputs.data, 1)
+                loss = criterion(outputs, labels)
+
+                if phase == "train":
+                    loss.backward()
+                    optimizer.step()
+
+                running_loss += loss.data[0]
+                running_corrects += torch.sum(preds == labels.data)
+
+                if phase == "train" and counter != 0 and counter % log_interval == 0:
+                    print("Training loss at iteration {}: {:.4f}".format(counter,
+                        running_loss / (counter * batch_size)))
+                    print("Training accuracy at iteration {}: {:.4f}".format(counter,
+                        running_corrects / (counter * batch_size)))
+
+
+                counter += 1
+
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects / len(dataloaders[phase].dataset)
+
+            print("{} Loss: {:.4f}  Acc: {:.4f}".format(phase, epoch_loss, epoch_acc))
+
+            if phase == "val" and epoch_acc > best_acc:
+                bast_acc = epoch_acc
+                best_model_wts = model.state_dict()
+
+        print()
+
+    time_elapsed = time.time() - since
+
+    print("Training completed in {:.0f}m {:.0f}s".format(
+        time_elapsed // 60, time_elapsed % 60))
+    print("Best validation accuracy: {:4f}".format(best_acc))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("c_type", type=str, help='type of classification to run [binary, flat]')
+    parser.add_argument("pretrain", type=bool, help='start with a pretrained model [1, 0]')
+    args = parser.parse_args()
+
+    classify(args.c_type, args.pretrain)
--- doubledataset.py
+++ doubledataset.py
@@ -6,16 +6,37 @@ from torch.utils.data import Dataset
 
 
 class DoubleDataSet(Dataset):
-    def __init__(self, fp1, fp2):
+    def __init__(self, fp1, fp2, which):
         """
         Constructor. Loads everything into memory, so be warned.
         :param fp1: filepath to first bson dataset.
         :param fp2: filepath to second bson dataset.
         """
+
+        fps = None
+
+        self.which = which
+
+        if which == "flat":
+            fps = [fp1, fp2]
+            self.num_classes = 40
+
+        elif which == "binary":
+            fps = [fp1, fp2]
+            self.num_classes = 2
+
+        elif which == "meuble":
+            fps = [fp1]
+            self.num_classes = 20
+
+        elif which == "electronique":
+            fps = [fp2]
+            self.num_classes = 20
+
         self.class_indexes = [[], []]  # [0] gives class 0 indices, [1] give class 1 indicies.
         # Pass this to a sampler in the DataLoader.
 
-        self.data, (self.binary_labels, self.categorical_labels) = self.parse_files(fp1, fp2)
+        self.data, (self.binary_labels, self.categorical_labels) = self.parse_files(fps)
         self.transform = torchvision.transforms.ToTensor()
 
     def __getitem__(self, item):
@@ -26,23 +47,30 @@ class DoubleDataSet(Dataset):
         """
         img = self.transform(Image.open(io.BytesIO(self.data[item])))
 
-        return img, self.binary_labels[item], self.categorical_labels[item]
+        if self.which in ["flat", "meuble", "electronique"]:
+            return img, self.categorical_labels[item]
+
+        elif self.which in ["binary"]:
+            return img, self.binary_labels[item]
+
+        else:
+            print("bad which")
+            exit(-1)
 
     def __len__(self):
         return len(self.data)
 
-    def parse_files(self, fp1, fp2):
+    def parse_files(self, fps):
         """
         Generate the data and label lists.
-        :param fp1: file path 1.
-        :param fp2: file path 2.
+        :param fp1s: list of file paths.
         :return:
         """
         data = []
         labels = []
 
         count = 0
-        for i, fp in enumerate([fp1, fp2]):
+        for i, fp in enumerate(fps):
             bson_data = bson.decode_file_iter(open(fp, 'rb'))
 
             fp_labels = []  # Labels for a specific file.
@@ -67,9 +95,9 @@ class DoubleDataSet(Dataset):
         :param label_list:
         :return:
         """
-        binary_labels = [0 for _ in label_list[0]] + [1 for _ in label_list[1]]
+        binary_labels = [i for i, lis in enumerate(label_list) for __ in lis]
 
-        label_list = label_list[0] + label_list[1]
+        label_list = [lab for lis in label_list for lab in lis]
 
         category_counts = {}
         count = 0
